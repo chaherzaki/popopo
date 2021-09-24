cd772b023d07840cef644b6ccb712c79d38d67c5
chaherzaki <chaherzaki@gmail.com>
2021-09-24 07:50:52 -0300

    Projetopo

---
+++ src/main/groovy/ProjetoZaki/codebackendaws/CodebackendAwsApplication.groovy
@@ -0,0 +1,13 @@
+package ProjetoZaki.codebackendaws
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CodebackendAwsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CodebackendAwsApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ProjetoZaki/codebackendaws/CodebackendAwsApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ProjetoZaki.codebackendaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodebackendAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
